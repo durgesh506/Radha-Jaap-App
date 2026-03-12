305d23340aabb49579ee24a37a18fb2515e11b6b
unknown <sharma50647@gmail.com>
2026-03-12 14:59:39 +0530

    fix pwa icon

--- vite.config.js
+++ vite.config.js
@@ -15,18 +15,18 @@ export default defineConfig({
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#ff6600',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.jpg',
-            sizes: '512x512',
-            type: 'image/jpg'
-          }
-        ]
+       icons: [
+  {
+    src: '/icon-192.png',
+    sizes: '192x192',
+    type: 'image/png'
+  },
+  {
+    src: '/icon-512.png',
+    sizes: '512x512',
+    type: 'image/png'
+  }
+]
       }
     })
   ]
